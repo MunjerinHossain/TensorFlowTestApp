72af496d66d9583194fc22dba7c28b76d44de4fa
Munjerin Hossain <munjerin15muna@gmail.com>
2021-02-10 03:19:55 +0600

    TF and toxicity model is loading.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Settings,
 } from 'react-native';
 
 import {
@@ -28,63 +29,108 @@ import {TextInput} from 'react-native';
 
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
+import * as toxicity from '@tensorflow-models/toxicity';
 
 // const App: () => React$Node = () => {
 
 // };
 
 // export class App extends React.Component {
-//   constructor(props) {
+// constructor(props) {
 //     super(props);
 //     this.state = {
 //       isTfReady: false,
 //     };
-//   }
- 
-//   async componentDidMount() {
+// }
+
+// async componentDidMount() {
 //     // Wait for tf to be ready.
 //     await tf.ready();
 //     // Signal to the app that tensorflow.js can now be used.
 //     this.setState({
 //       isTfReady: true,
 //     });
-//   }
- 
- 
-//   render() {
-//     //
-//   }
 // }
 
-export default function App(){
-  const [value, onChangeText] = React.useState('');
+// render() {
+//     //
+// }
+// }
+
+export default function App() {
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [isModelReady, setIsModelReady] = useState(false);
+  const [text, setText] = useState('');
+  const [prediction, setPrediction] = useState(null);
 
   // will test here for the sentiment analysis
-  
+  useEffect(() => {
+    const TFReady = tf.ready();
+    setIsTfReady(true);
+    // console.warn('tf is ready');
+
+    {
+      toxicity.load()
+        ? setIsModelReady(true)
+        : '';
+    }
+
+    // if(model == toxicity.load()){
+    //   setIsModelReady(true);
+    //   console.warn('model is ready');
+    // }
+  });
+
+  // The minimum prediction confidence.
+  const threshold = 0.9;
+
+  // Load the model. Users optionally pass in a threshold and an array of
+  // labels to include.
+  toxicity.load(threshold).then((model) => {
+    // const sentences = ['you suck'];
+
+    model.classify(text).then((predictions) => {
+      // `predictions` is an array of objects, one for each prediction head,
+      // that contains the raw probabilities for each input along with the
+      // final prediction in `match` (either `true` or `false`).
+      // If neither prediction exceeds the threshold, `match` is `null`.
+
+      console.log('prediction: ', predictions);
+    });
+  });
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+      <View style={styles.container}>
+        <Text>Sentiment Analysis</Text>
+        <Text>
+          TF ready?{' '}
+          {isTfReady ? (
+            <Text>Yes, TF loaded</Text>
+          ) : (
+            <Text>Loading TF...</Text>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}></View>
-            <Text>Write Something Here:</Text>
-            <TextInput
-              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-              onChangeText={(text) => onChangeText(text)}
-              value={value}
-            />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+        </Text>
+        <Text>
+          Model ready?{' '}
+          {isModelReady ? (
+            <Text>Yes, model loaded</Text>
+          ) : (
+            <Text>Loading Model...</Text>
+          )}
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Write your text here"
+          onChangeText={(val) => {
+            setText(val);
+            setPrediction(val.predictions)
+          }}
+        />
+
+        <Text>Written Text: {text}</Text>
+        <Text>Prediction: {prediction}</Text>
+      </View>
     </>
   );
 }
@@ -125,6 +171,19 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
